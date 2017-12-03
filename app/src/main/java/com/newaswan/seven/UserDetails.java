88aedd43d0e68bdefeba63c9d034a4a89eaa7317
Anonymo <mahmudamen@gmail.com>
2017-12-03 19:21:19 +0200

    commit l

---
+++ app/src/main/java/com/newaswan/seven/UserDetails.java
@@ -0,0 +1,11 @@
+package com.newaswan.seven;
+
+/**
+ * Created by Anonymo on 11/29/2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
